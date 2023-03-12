f394025f160f204371b8ce8f127c67b878927f8d
Rik <97382691+sontaimnt@users.noreply.github.com>
2023-03-12 18:09:01 +0530

    Create colors.py

---
+++ colors.py
@@ -0,0 +1,15 @@
+# =============
+# COLORS
+# =============
+#
+# This file shows the colors.. EDIT THIS VALUES AT THE OWN RISK
+
+bg="grey10"
+hoveredbg="grey15"
+fg = "antiquewhite"
+colorsred = ["firebrick1" , "firebrick3"]
+colorsorange = ["chocolate1" , "chocolate2"]
+colorsyellow = ["yellow1" , "yellow3"]
+colorsgreen = ["green1" , "green3"]
+colorsblue = ["dodgerblue1" , "dodgerblue4"]
+colorspurple = ["mediumorchid" , "mediumorchid4"]
