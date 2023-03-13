be3ead920fde4eaa02bf70582c12703029f17638
Rik <97382691+sontaimnt@users.noreply.github.com>
2023-03-13 14:03:11 +0530

    Update colors.py

--- colors.py
+++ colors.py
@@ -7,9 +7,9 @@
 bg="grey10"
 hoveredbg="grey15"
 fg = "antiquewhite"
-colorsred = ["firebrick1" , "firebrick3"]
+colorsred = ["firebrick1" , "firebrick4"]
 colorsorange = ["chocolate1" , "chocolate2"]
-colorsyellow = ["yellow1" , "yellow3"]
-colorsgreen = ["green1" , "green3"]
+colorsyellow = ["yellow1" , "yellow4"]
+colorsgreen = ["green1" , "green4"]
 colorsblue = ["dodgerblue1" , "dodgerblue4"]
 colorspurple = ["mediumorchid" , "mediumorchid4"]
